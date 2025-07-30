481b343c0eed37394d7ad7592df4507fa02380db
northys <northys@pm.me>
2025-07-30 20:23:37 +0200

    setup typescript project with eslint, jest and crawlee

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      }],
+      'no-console': 'warn',
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+    },
+  },
+];
